3cfb868a27375419195171becc11aa7786c5c15e
Lili <79230737+sailorlyx@users.noreply.github.com>
2022-10-06 17:53:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,73 @@
 ﻿using System;
-class calculadora
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chibicalcu
 {
-    static void Main()
+    public class Program
     {
-        double numerito1;
-        Console.WriteLine("¡bienvenido a la mini calculadora! dime un numerito, porfi");
-        numerito1 = Convert.ToDouble(
-            Console.ReadLine());
-
-        double numerito2;
-        Console.WriteLine("dime otro numerito, porfi");
-        numerito2 = Convert.ToDouble(
-            Console.ReadLine());
-
-        Console.WriteLine("¿qué operación te gustaría realizar");
-        Console.WriteLine("pulsa 1 para suma");
-        Console.WriteLine("pulsa 2 para resta");
-        Console.WriteLine("pulsa 3 para multiplicación");
-        Console.WriteLine("pulsa 4 para división");
-
-        switch (Console.ReadLine())
+        static string EjecutarCalculadora()
         {
-            case "1":
-                Console.WriteLine("el total es " + (numerito1 + numerito2));
-                break;
+            double numerito1 = PedirNumerito("¡bienvenido a la minicalculadora! dime un numerito porfi");
 
-            case "2":
-                Console.WriteLine("el total es " + (numerito1 - numerito2));
-                break;
+            double numerito2 = PedirNumerito("dime otro numerito, porfi");
 
-            case "3":
-                Console.WriteLine("el total es " + (numerito1 * numerito2));
-                break;
+            MenuCalculadora();
 
-            case "4":
-                Console.WriteLine("el total es " + (numerito1 / numerito2));
-                break;
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("el total es " + (numerito1 + numerito2));
+                    break;
 
-            default:
-                Console.WriteLine("ha habido un error, intentalo de nuevo!");
-                break;
+                case "2":
+                    Console.WriteLine("el total es " + (numerito1 - numerito2));
+                    break;
+
+                case "3":
+                    Console.WriteLine("el total es " + (numerito1 * numerito2));
+                    break;
+
+                case "4":
+                    Console.WriteLine("el total es " + (numerito1 / numerito2));
+                    break;
+
+                default:
+                    Console.WriteLine("ha habido un error, intentalo de nuevo!");
+                    break;
+            }
+            Console.WriteLine("¿te gustaría realizar otra operación? sip/nop");
+            string seguir = Console.ReadLine();
+            return seguir;
+        }
+        static void MenuCalculadora()
+        {
+            Console.WriteLine("¿qué operación te gustaría realizar");
+            Console.WriteLine("pulsa 1 para suma");
+            Console.WriteLine("pulsa 2 para resta");
+            Console.WriteLine("pulsa 3 para multiplicación");
+            Console.WriteLine("pulsa 4 para división");
+
+        }
+
+        public static double PedirNumerito(string peticion)
+        {
+            double numerito;
+            Console.WriteLine(peticion);
+            numerito = Convert.ToDouble(Console.ReadLine());
+            return numerito;
+        }
+        static void Main(string[] args)
+        {
+            string seguir = "sip";
+
+            while (seguir == "sip")
+            {
+                seguir = EjecutarCalculadora();
+            }
         }
     }
 }
